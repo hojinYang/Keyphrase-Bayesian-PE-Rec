d9ffd81bc1c66d8b7a525527249ae9cbb345e1fc
Jin <jpzhou01@gmail.com>
2020-08-21 13:41:02 -0400

    added alpha

--- utils/querynames.py
+++ utils/querynames.py
@@ -1,12 +1,12 @@
-from experiment.query_ranker import rank_by_mean, rank_by_variance, rank_by_UCB, rank_randomly,rank_by_TS,rank_by_POP,rank_by_PPOP
+from experiment.query_ranker import rank_by_mean, rank_by_variance, rank_by_UCB, rank_randomly, rank_by_TS, rank_by_POP, \
+    rank_by_PPOP
 
 queries = {
-    "Mean" : rank_by_mean,
+    "Mean": rank_by_mean,
     "Var": rank_by_variance,
-    "UCB" : rank_by_UCB,
-    "Random" : rank_randomly,
-    "TS" : rank_by_TS,
-    "POP" : rank_by_POP,
-    "PPOP" : rank_by_PPOP
+    "UCB": rank_by_UCB,
+    "Random": rank_randomly,
+    "TS": rank_by_TS,
+    "POP": rank_by_POP,
+    "PPOP": rank_by_PPOP
 }
-
